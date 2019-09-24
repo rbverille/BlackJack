c8d6030efaa09af27042dac3f3672966b54f4532
rbverille <31780108+rbverille@users.noreply.github.com>
2019-09-23 20:57:02 -0400

    Add files via upload

---
+++ BlackJack.java
@@ -0,0 +1,46 @@
+package com.example.ryeverille.blackjack;
+
+public class BlackJack {
+
+    //put all rules
+
+//has no cards
+    public boolean hasNoCards(){
+        return true;
+    }
+
+
+//blackjack --> cards == 21
+
+
+//players hand 2 cards
+
+
+//dealers hand 2 cards
+
+
+//hit = i want another card
+
+
+//stop = i dont want any other cards
+
+//after 3 hits the button is disabled bc game is over
+
+
+//player won
+    public boolean playerWon(){
+        return true;
+    }
+
+//dealer won
+    public boolean dealerWon(){
+        return true;
+    }
+
+//tie
+    public boolean tieGame(){
+        return true;
+    }
+
+
+}
